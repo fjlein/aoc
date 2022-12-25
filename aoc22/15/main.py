16bda3c4b68e6aa4857aee9edfeb6b6fb177a564
Felix <fjlein@gmail.com>
2022-12-25 17:33:50 +0900

    both parts work now
    suuuper slow :(

--- aoc22/15/main.py
+++ aoc22/15/main.py
@@ -22,17 +22,19 @@ Sensor at x=3991401, y=2367688: closest beacon is at x=3790542, y=2949630
 Sensor at x=3292286, y=2624894: closest beacon is at x=3279264, y=2775610
 Sensor at x=2194423, y=3990859: closest beacon is at x=2275951, y=3717327"""
 
-pairs = set()
-sensors = set()
-beacons = set()
 
-for line in input.split("\n"):
-    line = line.split(" ")
-    sensor = (int(line[2][2:-1]), int(line[3][2:-1]))
-    beacon = (int(line[8][2:-1]), int(line[9][2:]))
-    sensors.add(sensor)
-    beacons.add(beacon)
-    pairs.add((sensor, beacon))
+def get_triplet(input):
+    pairs = set()
+    sensors = set()
+    beacons = set()
+    for line in input.split("\n"):
+        line = line.split(" ")
+        sensor = (int(line[2][2:-1]), int(line[3][2:-1]))
+        beacon = (int(line[8][2:-1]), int(line[9][2:]))
+        sensors.add(sensor)
+        beacons.add(beacon)
+        pairs.add((sensor, beacon, manhatten(sensor, beacon)))
+    return pairs, sensors, beacons
 
 
 def manhatten(a, b):
@@ -41,22 +43,58 @@ def manhatten(a, b):
     return abs(by - ay) + abs(bx - ax)
 
 
-print(manhatten((0, 11), (2, 10)))
+def in_reach(point: (int, int), pairs) -> bool:
+    for p in pairs:
+        if manhatten(point, p[0]) <= p[2]:
+            return True
+    return False
 
-y = 2000000
 
-yy = set()
+def part1(pairs, sensors, beacons):
+    y = 2000000
 
-for p in pairs:
-    print("check", p)
-    sx, sy = p[0]
-    bx, by = p[1]
-    dis = manhatten(p[0], p[1])
-    if abs(sy - y) <= dis:
-        d = abs(dis - abs(sy - y))
-        for i in range(sx - d, sx + d + 1):
-            yy.add((i, y))
+    yy = set()
 
-valid = yy - beacons - sensors
+    for p in pairs:
+        sx, sy = p[0]
+        dis = manhatten(p[0], p[1])
+        if abs(sy - y) <= dis:
+            d = abs(dis - abs(sy - y))
+            for i in range(sx - d, sx + d + 1):
+                yy.add((i, y))
 
-print("len", len(valid))
+    valid = yy - beacons - sensors
+
+    return len(valid)
+
+
+def part2(pairs):
+    for p in pairs:
+        (sx, sy), beacon, dis = p
+        d = dis + 1
+        for i in range(-d, d + 1):
+            new_y = sy + i
+            if not 0 <= new_y <= 4000000:
+                continue
+            new_x_1 = sx + abs(d - abs(i))
+            new_x_2 = sx - abs(d - abs(i))
+            if 0 <= new_x_1 <= 4000000:
+                if not in_reach((new_x_1, new_y), pairs):
+                    return new_x_1 * 4000000 + new_y
+            if 0 <= new_x_2 <= 4000000:
+                if not in_reach((new_x_2, new_y), pairs):
+                    return new_x_2 * 4000000 + new_y
+        print("added candidates")
+    print("finished adding")
+
+
+def solve(input, part):
+    pairs, sensors, beacons = get_triplet(input)
+    if part == 1:
+        return part1(pairs, sensors, beacons)
+    if part == 2:
+        return part2(pairs)
+
+
+print("answer 1:", solve(input, part=1))
+print("answer 2:", solve(input, part=2))
