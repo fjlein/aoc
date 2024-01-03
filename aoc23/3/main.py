8e58488bc0a2ca3cff2d10de25e535f121a587dd
Felix Lein <fjlein@gmail.com>
2024-01-03 13:32:06 +0100

    clean up

--- aoc23/3/main.py
+++ aoc23/3/main.py
@@ -1,7 +1,8 @@
 def parse(puzzle_input):
     lines = [line.rstrip() for line in puzzle_input.split("\n")]
-    numbers, chars = set(), set()
+    numbers, chars = dict(), set()
     x, y = 0, 0
+
     while y < len(lines[0]):
         x = 0
         while x < len(lines):
@@ -11,65 +12,62 @@ def parse(puzzle_input):
                 while x < len(lines[0]) and y < len(lines) and lines[y][x].isnumeric():
                     number += lines[y][x]
                     x += 1
-                numbers.add((coords, number))
+                numbers[coords] = number
             else:
                 if lines[y][x] != ".":
-                    chars.add(((x, y), lines[y][x]))
+                    chars.add((x, y))
                 x += 1
         y += 1
-    return chars, numbers
+
+    return chars, numbers, lines
 
 
 def part1(data):
-    chars, numbers = data
-    char_positions = [(x, y) for ((x, y), _) in chars]
+    chars, numbers, lines = data
     sum = 0
+
     for n in numbers:
-        (x, y), number = n
-        neighbors = set([(x - 1, y), (x + len(number), y)])
-        for i in range(x - 1, x + len(number) + 1):
-            neighbors.add((i, y - 1))
-            neighbors.add((i, y + 1))
+        (x, y) = n
+        neighbors = set([(x - 1, y), (x + len(numbers[(x, y)]), y)])
+        for i in range(x - 1, x + len(numbers[(x, y)]) + 1):
+            neighbors.update([(i, y - 1), (i, y + 1)])
 
         has_neighbor = False
         for neighbor in neighbors:
-            if neighbor in char_positions:
+            if neighbor in chars:
                 has_neighbor = True
                 break
         if has_neighbor:
-            sum += int(number)
+            sum += int(numbers[(x, y)])
+
     return sum
 
 
 def part2(data):
-    chars, numbers = data
-    new_numbers = set()
-    for number in numbers:
-        (x, y), n = number
-        for i in range(len(n)):
-            new_numbers.add(((x + i, y), n))
-
-    new_numbers_coords = {(x, y) for ((x, y), _) in new_numbers}
-
+    chars, numbers, lines = data
+    new_numbers = dict()
     sum = 0
 
+    for number in numbers:
+        (x, y) = number
+        for i in range(len(numbers[(x, y)])):
+            new_numbers[(x + i, y)] = numbers[(x, y)]
+
     for char in chars:
-        (x, y), c = char
-        if c != "*":
+        (x, y) = char
+        if lines[y][x] != "*":
             continue
-        neighbor_numbers = set()
-        neighbors = set([(-1, -1), (0, -1), (1, -1), (-1, 1),
-                        (0, 1), (1, 1), (-1, 0), (1, 0)])
 
-        for neighbor in neighbors:
-            nx, ny = neighbor
-            if (x + nx, y + ny) in new_numbers_coords:
+        neighbors = set()
 
-                n = list(filter(lambda n: n[0][0] == x +
-                                nx and n[0][1] == y + ny, new_numbers))[0]
-                neighbor_numbers.add(n[1])
-        if len(neighbor_numbers) == 2:
-            a, b = list(neighbor_numbers)
+        for n in set([(-1, -1), (0, -1), (1, -1), (-1, 1),
+                      (0, 1), (1, 1), (-1, 0), (1, 0)]):
+            neighbor = (x + n[0], y + n[1])
+            if neighbor in new_numbers:
+                neighbors.add(new_numbers[neighbor])
+
+        if len(neighbors) == 2:
+            a, b = list(neighbors)
             sum += int(a) * int(b)
 
     return sum
