bd369e8047b4c27a2f49e2dce4dce0c75f65414d
Felix Lein <fjlein@gmail.com>
2024-01-03 14:17:31 +0100

    clean up

--- aoc23/4/main.py
+++ aoc23/4/main.py
@@ -7,20 +7,19 @@ def parse(puzzle_input):
 
     data = []
     for line in lines:
-        data.append([list(map(int, re.findall(" ..", l)))
+        data.append([set(map(int, re.findall(" ..", l)))
                     for l in line.split(":")[1].split(" |")])
     return data
 
 
 def part1(data):
-    sum = 0
+    points = []
 
     for card in data:
         winning, my = card
-        wins = list(filter(lambda x: x in winning, my))
-        sum += math.floor(2**(len(wins) - 1))
-
-    return sum
+        wins = len(set(filter(lambda x: x in winning, my)))
+        points.append(math.floor(2**(wins - 1)))
+    return sum(points)
 
 
 def part2(data):
@@ -28,10 +27,9 @@ def part2(data):
 
     for i, card in enumerate(data):
         winning, my = card
-        l = len(list(filter(lambda x: x in winning, my)))
-        for k in range(scratchcards[i]):
-            for j in range(i + 1, i + l + 1):
-                scratchcards[j] += 1
+        wins = len(set(filter(lambda x: x in winning, my)))
+        for j in range(i + 1, i + wins + 1):
+            scratchcards[j] += scratchcards[i]
     return sum(scratchcards)
 
 
