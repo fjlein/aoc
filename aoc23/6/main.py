fe09d250c8b5a4bd54b728866acabfc3112438a7
Felix Lein <fjlein@gmail.com>
2024-01-06 21:17:29 +0100

    init puzzle 6

---
+++ aoc23/6/main.py
@@ -0,0 +1,26 @@
+
+def parse(puzzle_input):
+    return 0
+
+
+def part1(data):
+    return 1
+
+
+def part2(data):
+    return 2
+
+
+def solve(puzzle_input):
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    with open('input.txt') as f:
+        puzzle_input = f.read()
+    solution1, solution2 = solve(puzzle_input)
+    print(f"{solution1}\n{solution2}")
